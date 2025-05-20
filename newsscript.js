6ce33a91545a7bf43d5ffa29d833836b17a44285
mattc7660 <mdcking4749@live.com>
2025-05-20 16:26:31 -0400

    image fix for news page

--- newsscript.js
+++ newsscript.js
@@ -2,25 +2,25 @@ const articles = [
     {
       title: "NYC Subway Safety",
       description: "How we can make everyone safer in the subway",
-      image: "imagesWD/safety.jpeg",
+      image: "img/safety.jpeg",
       link: "https://www.fastcompany.com/91333588/this-simple-change-could-make-nycs-subway-feel-safer"
     },
     {
       title: "Subway News",
       description: "Latest updates about NYC subways",
-      image: "imagesWD/6train.jpeg",
+      image: "img/6train.jpeg",
       link: "https://www.cbsnews.com/newyork/tag/subway/"
     },
     {
       title: "Subway Dangers",
       description: "Checklist to help you stay safe",
-      image: "imagesWD/turnstile.jpeg",
+      image: "img/turnstile.jpeg",
       link: "https://gothamist.com/news/feeling-anxious-about-riding-the-nyc-subway-heres-a-guide-for-staying-safe-underground"
     },
     {
       title: "Train Status",
       description: "Check any updates on train lines you need.",
-      image: "imagesWD/status.jpeg",
+      image: "img/status.jpeg",
       link: "https://subwaystats.com/#google_vignette"
     }
   ];
